4316536dd00d4aebf84553c3be5df78f9b4c0500
Tanmay Maheshwari <tanmaym06@proton.me>
2026-08-27 11:54:53 +0530

    refactor(regular opcode blocks): Refactor of the regular opcode execution engine
    - Removed the declaration and defintion of instructions that have a
    dedicated dispatcher for them.
    - Removed those instructions from the instr_table in cpu_tables.c
    - cpu_execute() now checks the opcode, and invokes the dispatcher if it
    has a dedicated dispatcher.
    
    Closes #41

--- src/core/cpu/cpu_tables.c
+++ src/core/cpu/cpu_tables.c
@@ -2,6 +2,7 @@
 #include <core/cpu/cpu.h>
 #include <core/cpu/cpu_exec.h>
 #include <core/bus.h>
+#include <core/utils.h>
 #include <gbemu.h>
 #include <stdio.h>
 
@@ -9,23 +10,17 @@
 // Instruction table (256 entries)
 // https://www.pastraiser.com/cpu/gameboy/gameboy_opcodes.html
 // ---------------------------------------------
-static const InstrFunc instr_table[256] = {
+static const InstrFunc instr_table[] = {
     // 0x0_
     [0x00] = instr_nop,
     [0x01] = instr_ld_bc_nn,
     [0x02] = instr_ld_mem_bc_a,
     [0x03] = instr_inc_bc,
-    [0x04] = instr_inc_b,
-    [0x05] = instr_dec_b,
-    [0x06] = instr_ld_b_n,
     [0x07] = instr_rlca,
     [0x08] = instr_ld_mem_a16_sp,
     [0x09] = instr_add_hl_bc,
     [0x0A] = instr_ld_a_mem_bc,
     [0x0B] = instr_dec_bc,
-    [0x0C] = instr_inc_c,
-    [0x0D] = instr_dec_c,
-    [0x0E] = instr_ld_c_n,
     [0x0F] = instr_rrca,
 
     // 0x1_
@@ -33,17 +28,11 @@ static const InstrFunc instr_table[256] = {
     [0x11] = instr_ld_de_nn,
     [0x12] = instr_ld_mem_de_a,
     [0x13] = instr_inc_de,
-    [0x14] = instr_inc_d,
-    [0x15] = instr_dec_d,
-    [0x16] = instr_ld_d_n,
     [0x17] = instr_rla,
     [0x18] = instr_jr_e8,
     [0x19] = instr_add_hl_de,
     [0x1A] = instr_ld_a_mem_de,
     [0x1B] = instr_dec_de,
-    [0x1C] = instr_inc_e,
-    [0x1D] = instr_dec_e,
-    [0x1E] = instr_ld_e_n,
     [0x1F] = instr_rra,
 
     // 0x2_
@@ -51,17 +40,11 @@ static const InstrFunc instr_table[256] = {
     [0x21] = instr_ld_hl_nn,
     [0x22] = instr_ld_mem_hli_a,
     [0x23] = instr_inc_hl,
-    [0x24] = instr_inc_h,
-    [0x25] = instr_dec_h,
-    [0x26] = instr_ld_h_n,
     [0x27] = instr_daa,
     [0x28] = instr_jr_z_e8,
     [0x29] = instr_add_hl_hl,
     [0x2A] = instr_ld_a_mem_hli,
     [0x2B] = instr_dec_hl,
-    [0x2C] = instr_inc_l,
-    [0x2D] = instr_dec_l,
-    [0x2E] = instr_ld_l_n,
     [0x2F] = instr_cpl,
 
     // 0x3_
@@ -69,162 +52,21 @@ static const InstrFunc instr_table[256] = {
     [0x31] = instr_ld_sp_nn,
     [0x32] = instr_ld_mem_hld_a,
     [0x33] = instr_inc_sp,
-    [0x34] = instr_inc_mem_hl,
-    [0x35] = instr_dec_mem_hl,
-    [0x36] = instr_ld_mem_hl_n,
     [0x37] = instr_scf,
     [0x38] = instr_jr_c_e8,
     [0x39] = instr_add_hl_sp,
     [0x3A] = instr_ld_a_mem_hld,
     [0x3B] = instr_dec_sp,
-    [0x3C] = instr_inc_a,
-    [0x3D] = instr_dec_a,
-    [0x3E] = instr_ld_a_n,
     [0x3F] = instr_ccf,
 
-    // 0x4_
-    [0x40] = instr_ld_b_b,
-    [0x41] = instr_ld_b_c,
-    [0x42] = instr_ld_b_d,
-    [0x43] = instr_ld_b_e,
-    [0x44] = instr_ld_b_h,
-    [0x45] = instr_ld_b_l,
-    [0x46] = instr_ld_b_mem_hl,
-    [0x47] = instr_ld_b_a,
-    [0x48] = instr_ld_c_b,
-    [0x49] = instr_ld_c_c,
-    [0x4A] = instr_ld_c_d,
-    [0x4B] = instr_ld_c_e,
-    [0x4C] = instr_ld_c_h,
-    [0x4D] = instr_ld_c_l,
-    [0x4E] = instr_ld_c_mem_hl,
-    [0x4F] = instr_ld_c_a,
+    // 0x4_ - 0x7_: LD r8,r8'
+    // Block handled by: instr_ld_r8_r8_or_mem_hl() in cpu_execute()
+    // 0x76 = HALT is an exception
 
-    // 0x5_
-    [0x50] = instr_ld_d_b,
-    [0x51] = instr_ld_d_c,
-    [0x52] = instr_ld_d_d,
-    [0x53] = instr_ld_d_e,
-    [0x54] = instr_ld_d_h,
-    [0x55] = instr_ld_d_l,
-    [0x56] = instr_ld_d_mem_hl,
-    [0x57] = instr_ld_d_a,
-    [0x58] = instr_ld_e_b,
-    [0x59] = instr_ld_e_c,
-    [0x5A] = instr_ld_e_d,
-    [0x5B] = instr_ld_e_e,
-    [0x5C] = instr_ld_e_h,
-    [0x5D] = instr_ld_e_l,
-    [0x5E] = instr_ld_e_mem_hl,
-    [0x5F] = instr_ld_e_a,
-
-    // 0x6_
-    [0x60] = instr_ld_h_b,
-    [0x61] = instr_ld_h_c,
-    [0x62] = instr_ld_h_d,
-    [0x63] = instr_ld_h_e,
-    [0x64] = instr_ld_h_h,
-    [0x65] = instr_ld_h_l,
-    [0x66] = instr_ld_h_mem_hl,
-    [0x67] = instr_ld_h_a,
-    [0x68] = instr_ld_l_b,
-    [0x69] = instr_ld_l_c,
-    [0x6A] = instr_ld_l_d,
-    [0x6B] = instr_ld_l_e,
-    [0x6C] = instr_ld_l_h,
-    [0x6D] = instr_ld_l_l,
-    [0x6E] = instr_ld_l_mem_hl,
-    [0x6F] = instr_ld_l_a,
-
-    // 0x7_
-    [0x70] = instr_ld_mem_hl_b,
-    [0x71] = instr_ld_mem_hl_c,
-    [0x72] = instr_ld_mem_hl_d,
-    [0x73] = instr_ld_mem_hl_e,
-    [0x74] = instr_ld_mem_hl_h,
-    [0x75] = instr_ld_mem_hl_l,
     [0x76] = instr_halt,
-    [0x77] = instr_ld_mem_hl_a,
-    [0x78] = instr_ld_a_b,
-    [0x79] = instr_ld_a_c,
-    [0x7A] = instr_ld_a_d,
-    [0x7B] = instr_ld_a_e,
-    [0x7C] = instr_ld_a_h,
-    [0x7D] = instr_ld_a_l,
-    [0x7E] = instr_ld_a_mem_hl,
-    [0x7F] = instr_ld_a_a,
 
-    // 0x8_
-    [0x80] = instr_add_a_b,
-    [0x81] = instr_add_a_c,
-    [0x82] = instr_add_a_d,
-    [0x83] = instr_add_a_e,
-    [0x84] = instr_add_a_h,
-    [0x85] = instr_add_a_l,
-    [0x86] = instr_add_a_mem_hl,
-    [0x87] = instr_add_a_a,
-    [0x88] = instr_adc_a_b,
-    [0x89] = instr_adc_a_c,
-    [0x8A] = instr_adc_a_d,
-    [0x8B] = instr_adc_a_e,
-    [0x8C] = instr_adc_a_h,
-    [0x8D] = instr_adc_a_l,
-    [0x8E] = instr_adc_a_mem_hl,
-    [0x8F] = instr_adc_a_a,
-
-    // 0x9_
-    [0x90] = instr_sub_a_b,
-    [0x91] = instr_sub_a_c,
-    [0x92] = instr_sub_a_d,
-    [0x93] = instr_sub_a_e,
-    [0x94] = instr_sub_a_h,
-    [0x95] = instr_sub_a_l,
-    [0x96] = instr_sub_a_mem_hl,
-    [0x97] = instr_sub_a_a,
-    [0x98] = instr_sbc_a_b,
-    [0x99] = instr_sbc_a_c,
-    [0x9A] = instr_sbc_a_d,
-    [0x9B] = instr_sbc_a_e,
-    [0x9C] = instr_sbc_a_h,
-    [0x9D] = instr_sbc_a_l,
-    [0x9E] = instr_sbc_a_mem_hl,
-    [0x9F] = instr_sbc_a_a,
-
-    // 0xA_
-    [0xA0] = instr_and_a_b,
-    [0xA1] = instr_and_a_c,
-    [0xA2] = instr_and_a_d,
-    [0xA3] = instr_and_a_e,
-    [0xA4] = instr_and_a_h,
-    [0xA5] = instr_and_a_l,
-    [0xA6] = instr_and_a_mem_hl,
-    [0xA7] = instr_and_a_a,
-    [0xA8] = instr_xor_a_b,
-    [0xA9] = instr_xor_a_c,
-    [0xAA] = instr_xor_a_d,
-    [0xAB] = instr_xor_a_e,
-    [0xAC] = instr_xor_a_h,
-    [0xAD] = instr_xor_a_l,
-    [0xAE] = instr_xor_a_mem_hl,
-    [0xAF] = instr_xor_a_a,
-
-    // 0xB_
-    [0xB0] = instr_or_a_b,
-    [0xB1] = instr_or_a_c,
-    [0xB2] = instr_or_a_d,
-    [0xB3] = instr_or_a_e,
-    [0xB4] = instr_or_a_h,
-    [0xB5] = instr_or_a_l,
-    [0xB6] = instr_or_a_mem_hl,
-    [0xB7] = instr_or_a_a,
-    [0xB8] = instr_cp_a_b,
-    [0xB9] = instr_cp_a_c,
-    [0xBA] = instr_cp_a_d,
-    [0xBB] = instr_cp_a_e,
-    [0xBC] = instr_cp_a_h,
-    [0xBD] = instr_cp_a_l,
-    [0xBE] = instr_cp_a_mem_hl,
-    [0xBF] = instr_cp_a_a,
+    // 0x8_ - 0xB_: ALU A,r8
+    // Block handled by: instr_alu_a_r8_or_mem_hl
 
     // 0xC_
     [0xC0] = instr_ret_nz,
@@ -233,7 +75,6 @@ static const InstrFunc instr_table[256] = {
     [0xC3] = instr_jp_a16,
     [0xC4] = instr_call_nz_a16,
     [0xC5] = instr_push_bc,
-    [0xC6] = instr_add_a_n,
     [0xC7] = instr_rst_00,
     [0xC8] = instr_ret_z,
     [0xC9] = instr_ret,
@@ -241,7 +82,6 @@ static const InstrFunc instr_table[256] = {
     [0xCB] = instr_prefix_cb,
     [0xCC] = instr_call_z_a16,
     [0xCD] = instr_call_a16,
-    [0xCE] = instr_adc_a_n,
     [0xCF] = instr_rst_08,
 
     // 0xD_
@@ -251,7 +91,6 @@ static const InstrFunc instr_table[256] = {
     [0xD3] = NULL,
     [0xD4] = instr_call_nc_a16,
     [0xD5] = instr_push_de,
-    [0xD6] = instr_sub_a_n,
     [0xD7] = instr_rst_10,
     [0xD8] = instr_ret_c,
     [0xD9] = instr_reti,
@@ -259,7 +98,6 @@ static const InstrFunc instr_table[256] = {
     [0xDB] = NULL,
     [0xDC] = instr_call_c_a16,
     [0xDD] = NULL,
-    [0xDE] = instr_sbc_a_n,
     [0xDF] = instr_rst_18,
 
     // 0xE_
@@ -269,7 +107,6 @@ static const InstrFunc instr_table[256] = {
     [0xE3] = NULL,
     [0xE4] = NULL,
     [0xE5] = instr_push_hl,
-    [0xE6] = instr_and_a_n,
     [0xE7] = instr_rst_20,
     [0xE8] = instr_add_sp_e8,
     [0xE9] = instr_jp_hl,
@@ -277,7 +114,6 @@ static const InstrFunc instr_table[256] = {
     [0xEB] = NULL,
     [0xEC] = NULL,
     [0xED] = NULL,
-    [0xEE] = instr_xor_a_n,
     [0xEF] = instr_rst_28,
 
     // 0xF_
@@ -287,7 +123,6 @@ static const InstrFunc instr_table[256] = {
     [0xF3] = instr_di,
     [0xF4] = NULL,
     [0xF5] = instr_push_af,
-    [0xF6] = instr_or_a_n,
     [0xF7] = instr_rst_30,
     [0xF8] = instr_ld_hl_sp_e8,
     [0xF9] = instr_ld_sp_hl,
@@ -295,7 +130,6 @@ static const InstrFunc instr_table[256] = {
     [0xFB] = instr_ei,
     [0xFC] = NULL,
     [0xFD] = NULL,
-    [0xFE] = instr_cp_a_n,
     [0xFF] = instr_rst_38,
 };
 
@@ -304,7 +138,43 @@ static const InstrFunc instr_table[256] = {
 // Called by cpu_step()
 // ---------------------------------------------
 u8 cpu_execute(CPU *cpu, u8 opcode) {
-    // Check if instruction is implemented
+
+    // Instructions: LD r8, n
+    // 8 Opcodes: (0x06,0x0E,0x16,0x1E,0x26,0x2E,0x3E & 0x36)
+    // Encoding: 00 rrr 110
+    if (MASK_BITS(opcode, 0xC7) == 0x06)
+        return instr_ld_r8_or_mem_hl_n(cpu, opcode);
+
+    // Instructions: LD r8, r8'
+    // 63 Opcodes: (0x40-0x7F except 0x76=HALT)
+    // Encoding: 01 ddd sss
+    if (opcode >= 0x40 && opcode <= 0x7F && opcode != 0x76)
+        return instr_ld_r8_r8_or_mem_hl(cpu, opcode);
+
+    // Instructions: INC r8
+    // 8 Opcodes: (0x04,0x14,0x24,0x34,0x0C,0x1C,0x2C,0x3C)
+    // Encoding: 00 rrr 100
+    if (MASK_BITS(opcode, 0xC7) == 0x04)
+        return instr_inc_r8_or_mem_hl(cpu, opcode);
+
+    // Instructions: DEC r8
+    // 8 Opcodes: (0x05,0x15,0x25,0x35,0x0D,0x1D,0x2D,0x3D)
+    // Encoding: 00 rrr 101
+    if (MASK_BITS(opcode, 0xC7) == 0x05)
+        return instr_dec_r8_or_mem_hl(cpu, opcode);
+
+    // Instructions: ALU A, n
+    // 8 Opcodes: (0xC6,0xD6,0xE6,0xF6,0xCE,0xDE,0xEE,0xFE)
+    // Encoding: 11 ooo 110
+    if (MASK_BITS(opcode, 0xC7) == 0xC6)
+        return instr_alu_a_n(cpu, opcode);
+
+    // Instructions: ALU A, r8
+    // 64 Opcodes: (0x80-0xBF)
+    // Encoding: 10 ooo sss
+    if (opcode >= 0x80 && opcode <= 0xBF)
+        return instr_alu_a_r8_or_mem_hl(cpu, opcode);
+
     if (instr_table[opcode] == NULL) {
         put_error("Illegal operation code: 0x%02x at PC = 0x%04x\n", opcode, cpu->pc - 1);
         return ILLEGAL;
